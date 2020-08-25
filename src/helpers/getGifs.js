bc3d53555d8f195a3a1ebbffea3e4f0ddfde641d
Danilo Caro <danilocaro21@gmail.com>
2020-08-25 19:10:45 -0400

    modificación llamada https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -3,7 +3,8 @@
 
 export const getGifs = async (category) => {
     const apiKey = 'd5dVqDKG5ohwi0FUtI1tPEWZ6Toidte6';
-    const request = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURI(category)}&limit=10`);
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURI(category)}&limit=10`;
+    const request = await fetch(url);
     const response = await request.json();
 
     return response.data.map( item => {
