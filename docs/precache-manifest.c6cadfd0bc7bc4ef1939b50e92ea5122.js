258c182989259b4dfd443e13941f0603c34c1ea2
Danilo Caro <danilocaro21@gmail.com>
2020-08-25 18:55:23 -0400

    renombre carpeta docs para git pages

---
+++ docs/precache-manifest.c6cadfd0bc7bc4ef1939b50e92ea5122.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9626f153c74a5dac6ad426ed1642844",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7718e3fa4631243ecb5",
+    "url": "/static/css/main.800de1de.chunk.css"
+  },
+  {
+    "revision": "6b1578778fd38a87f0cb",
+    "url": "/static/js/2.a685c4d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a685c4d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7718e3fa4631243ecb5",
+    "url": "/static/js/main.34082f51.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
