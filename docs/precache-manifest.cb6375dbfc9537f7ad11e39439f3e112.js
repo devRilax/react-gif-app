cd2e73a40fa164ad0282babc75146ab8cb42408e
Danilo Caro <danilocaro21@gmail.com>
2020-08-25 19:13:30 -0400

    index.js edit

---
+++ docs/precache-manifest.cb6375dbfc9537f7ad11e39439f3e112.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "104910e816f359e43145e30cdd6cb02c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "657732eb3209f0b87c90",
+    "url": "/static/css/main.800de1de.chunk.css"
+  },
+  {
+    "revision": "6b1578778fd38a87f0cb",
+    "url": "/static/js/2.a685c4d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a685c4d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "657732eb3209f0b87c90",
+    "url": "/static/js/main.3f89fdcf.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
